eb3fa154c49cd0972eb307f215f81930335a972e
Vandy Liu <vandy.liu@astronomer.io>
2024-04-14 23:51:17 -0700

    fix

--- internal/provider/provider_test_utils.go
+++ internal/provider/provider_test_utils.go
@@ -3,6 +3,7 @@ package provider
 import (
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
@@ -21,20 +22,24 @@ func TestAccPreCheck(t *testing.T) {
 	// You can add code here to run prior to any test case execution, for example assertions
 	// about the appropriate environment variables being set are common to see in a pre-check
 	// function.
+	var errMsgs []string
 	if hostedToken := os.Getenv("HOSTED_ORGANIZATION_API_TOKEN"); len(hostedToken) == 0 {
-		t.Fatal("HOSTED_ORGANIZATION_API_TOKEN must be set for acceptance tests")
+		errMsgs = append(errMsgs, "HOSTED_ORGANIZATION_API_TOKEN must be set for acceptance tests")
 	}
 	if hostedOrgId := os.Getenv("HOSTED_ORGANIZATION_ID"); len(hostedOrgId) == 0 {
-		t.Fatal("HOSTED_ORGANIZATION_ID must be set for acceptance tests")
+		errMsgs = append(errMsgs, "HOSTED_ORGANIZATION_ID must be set for acceptance tests")
 	}
 	if hybridToken := os.Getenv("HYBRID_ORGANIZATION_API_TOKEN"); len(hybridToken) == 0 {
-		t.Fatal("HYBRID_ORGANIZATION_API_TOKEN must be set for acceptance tests")
+		errMsgs = append(errMsgs, "HYBRID_ORGANIZATION_API_TOKEN must be set for acceptance tests")
 	}
 	if hybridOrgId := os.Getenv("HYBRID_ORGANIZATION_ID"); len(hybridOrgId) == 0 {
-		t.Fatal("HYBRID_ORGANIZATION_ID must be set for acceptance tests")
+		errMsgs = append(errMsgs, "HYBRID_ORGANIZATION_ID must be set for acceptance tests")
 	}
 	if host := os.Getenv("ASTRO_API_HOST"); len(host) == 0 {
-		t.Fatal("ASTRO_API_HOST must be set for acceptance tests")
+		errMsgs = append(errMsgs, "ASTRO_API_HOST must be set for acceptance tests")
+	}
+	if len(errMsgs) > 0 {
+		t.Fatalf("Pre-check failed: %+v", strings.Join(errMsgs, ", "))
 	}
 }
 
