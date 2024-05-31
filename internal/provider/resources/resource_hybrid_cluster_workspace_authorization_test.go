dffaad50aec149bc0c99101d99ccb4c5c7ff039f
Isaac Chung <isaacchung1990@gmail.com>
2024-05-30 17:41:17 -0700

    added tests

---
+++ internal/provider/resources/resource_hybrid_cluster_workspace_authorization_test.go
@@ -0,0 +1,119 @@
+package resources_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/astronomer/terraform-provider-astro/internal/clients"
+	astronomerprovider "github.com/astronomer/terraform-provider-astro/internal/provider"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/stretchr/testify/assert"
+
+	"os"
+	"testing"
+
+	"github.com/astronomer/terraform-provider-astro/internal/utils"
+)
+
+func TestAcc_ResourceHybridClusterWorkspaceAuthorization(t *testing.T) {
+	namePrefix := utils.GenerateTestResourceName(10)
+
+	workspaceName := fmt.Sprintf("%v_workspace", namePrefix)
+	workspaceResourceVar := fmt.Sprintf("astro_workspace.%v", workspaceName)
+	hybridWorkspaceId := os.Getenv("HYBRID_WORKSPACE_ID")
+
+	clusterId := os.Getenv("HYBRID_CLUSTER_ID")
+	clusterWorkspaceAuth := fmt.Sprintf("%v_auth", namePrefix)
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: astronomerprovider.TestAccProtoV6ProviderFactories,
+		PreCheck:                 func() { astronomerprovider.TestAccPreCheck(t) },
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			// Check that cluster workspace authorizations have been removed
+			testAccCheckHybridClusterWorkspaceAuthorizationExistence(t, clusterWorkspaceAuth, false),
+		),
+		Steps: []resource.TestStep{
+			{
+				Config: astronomerprovider.ProviderConfig(t, true) +
+					workspace(workspaceName, workspaceName, utils.TestResourceDescription, false) +
+					hybridClusterWorkspaceAuthorization(hybridClusterWorkspaceAuthorizationInput{
+						Name:         clusterWorkspaceAuth,
+						ClusterId:    clusterId,
+						WorkspaceIds: []string{hybridWorkspaceId, fmt.Sprintf("%v.id", workspaceResourceVar)},
+					}),
+				Check: resource.ComposeTestCheckFunc(
+					// Check hybrid cluster workspace authorization
+					resource.TestCheckResourceAttr(clusterWorkspaceAuth, "cluster_id", clusterId),
+					resource.TestCheckResourceAttr(clusterWorkspaceAuth, "workspace_ids.#", "2"),
+
+					testAccCheckHybridClusterWorkspaceAuthorizationExistence(t, clusterWorkspaceAuth, true),
+				),
+			},
+			// Remove workspace from cluster workspace authorization
+			{
+				Config: astronomerprovider.ProviderConfig(t, true) +
+					hybridClusterWorkspaceAuthorization(hybridClusterWorkspaceAuthorizationInput{
+						Name:         clusterWorkspaceAuth,
+						ClusterId:    clusterId,
+						WorkspaceIds: []string{hybridWorkspaceId},
+					}),
+				Check: resource.ComposeTestCheckFunc(
+					// Check hybrid cluster workspace authorization
+					resource.TestCheckResourceAttr(clusterWorkspaceAuth, "cluster_id", clusterId),
+					resource.TestCheckResourceAttr(clusterWorkspaceAuth, "workspace_ids.#", "1"),
+
+					testAccCheckHybridClusterWorkspaceAuthorizationExistence(t, clusterWorkspaceAuth, true),
+				),
+			},
+		},
+	})
+}
+
+type hybridClusterWorkspaceAuthorizationInput struct {
+	Name         string
+	ClusterId    string
+	WorkspaceIds []string
+}
+
+func hybridClusterWorkspaceAuthorization(input hybridClusterWorkspaceAuthorizationInput) string {
+	return fmt.Sprintf(`
+		resource "astro_cluster_workspace_authorization" "%s" {
+			cluster_id = "%s"
+			workspace_ids = %v
+		}`, input.Name, input.ClusterId, input.WorkspaceIds)
+}
+
+func testAccCheckHybridClusterWorkspaceAuthorizationExistence(t *testing.T, name string, shouldExist bool) func(state *terraform.State) error {
+	t.Helper()
+	return func(state *terraform.State) error {
+		client, err := utils.GetTestHostedPlatformClient()
+		assert.NoError(t, err)
+
+		organizationId := os.Getenv("HYBRID_ORGANIZATION_ID")
+		clusterId := os.Getenv("HYBRID_CLUSTER_ID")
+
+		ctx := context.Background()
+		resp, err := client.GetClusterWithResponse(ctx, organizationId, clusterId)
+		if err != nil {
+			return fmt.Errorf("failed to get cluster: %w", err)
+		}
+		if resp == nil {
+			return fmt.Errorf("response is nil")
+		}
+		if resp.JSON200 == nil {
+			status, diag := clients.NormalizeAPIError(ctx, resp.HTTPResponse, resp.Body)
+			return fmt.Errorf("response JSON200 is nil status: %v, err: %v", status, diag.Detail())
+		}
+		if shouldExist {
+			if len(*resp.JSON200.WorkspaceIds) != 1 {
+				return fmt.Errorf("cluster workspace authorization %s should exist", name)
+			}
+		} else {
+			if len(*resp.JSON200.WorkspaceIds) != 0 {
+				return fmt.Errorf("cluster workspace authorization %s should not exist", name)
+			}
+		}
+		return nil
+	}
+}
