ccebf026a7c8e99ff36288116366f94801bf8bc9
Vandy Liu <vandy.liu@astronomer.io>
2024-05-31 12:27:30 -0700

    add more comments

--- examples/resources/astro_cluster/resource.tf
+++ examples/resources/astro_cluster/resource.tf
@@ -38,8 +38,8 @@ resource "astro_cluster" "gcp_example" {
 
 // Import an existing cluster
 import = {
-    id = "clozc036j01to01jrlgvuf98d"
-    to = astro_cluster.imported_cluster
+  id = "clozc036j01to01jrlgvuf98d" // CUID of the existing cluster
+  to = astro_cluster.imported_cluster
 }
 resource "astro_cluster" "imported_cluster" {
   type                  = "DEDICATED"
