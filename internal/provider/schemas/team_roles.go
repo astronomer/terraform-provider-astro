b91c51fd9f6a606cb9573f30ed3e01b4e3629fe9
Vandy Liu <33995460+vandyliu@users.noreply.github.com>
2024-06-03 15:28:57 -0700

    Add astro team roles resource for workspace/deployment-team memberships (#69)
    Co-authored-by: Isaac Chung <69920967+ichung08@users.noreply.github.com>

---
+++ internal/provider/schemas/team_roles.go
@@ -0,0 +1,53 @@
+package schemas
+
+import (
+	"github.com/astronomer/terraform-provider-astro/internal/clients/iam"
+	"github.com/astronomer/terraform-provider-astro/internal/provider/validators"
+	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
+	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+func ResourceTeamRolesSchemaAttributes() map[string]resourceSchema.Attribute {
+	return map[string]resourceSchema.Attribute{
+		"team_id": resourceSchema.StringAttribute{
+			MarkdownDescription: "The ID of the team to assign the roles to",
+			Required:            true,
+			Validators: []validator.String{
+				validators.IsCuid(),
+			},
+		},
+		"organization_role": resourceSchema.StringAttribute{
+			MarkdownDescription: "The role to assign to the organization",
+			Required:            true,
+			Validators: []validator.String{
+				stringvalidator.OneOf(
+					string(iam.ORGANIZATIONOWNER),
+					string(iam.ORGANIZATIONMEMBER),
+					string(iam.ORGANIZATIONBILLINGADMIN),
+				),
+			},
+		},
+		"workspace_roles": resourceSchema.SetNestedAttribute{
+			NestedObject: resourceSchema.NestedAttributeObject{
+				Attributes: ResourceWorkspaceRoleSchemaAttributes(),
+			},
+			Optional:            true,
+			MarkdownDescription: "The roles to assign to the workspaces",
+			Validators: []validator.Set{
+				setvalidator.SizeAtLeast(1),
+			},
+		},
+		"deployment_roles": resourceSchema.SetNestedAttribute{
+			NestedObject: resourceSchema.NestedAttributeObject{
+				Attributes: ResourceDeploymentRoleSchemaAttributes(),
+			},
+			Optional:            true,
+			MarkdownDescription: "The roles to assign to the deployments",
+			Validators: []validator.Set{
+				setvalidator.SizeAtLeast(1),
+			},
+		},
+	}
+}
