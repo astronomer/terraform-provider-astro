d29274444a3fe94f8ed3217c9f9764aba6461786
Isaac Chung <isaacchung1990@gmail.com>
2024-06-03 23:26:57 -0700

    change schema validator

--- internal/provider/schemas/hybrid_cluster_workspace_authorization.go
+++ internal/provider/schemas/hybrid_cluster_workspace_authorization.go
@@ -2,7 +2,6 @@ package schemas
 
 import (
 	"github.com/astronomer/terraform-provider-astro/internal/provider/validators"
-	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	resourceSchema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -21,9 +20,6 @@ func ResourceHybridClusterWorkspaceAuthorizationSchemaAttributes() map[string]re
 			ElementType:         types.StringType,
 			MarkdownDescription: "The IDs of the workspaces to authorize for the hybrid cluster",
 			Optional:            true,
-			Validators: []validator.Set{
-				setvalidator.SizeAtLeast(1),
-			},
 		},
 	}
 }
