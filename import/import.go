e2f38b5567085298675eae49c5b10c9c9fb31616
Isaac Chung <isaacchung1990@gmail.com>
2024-08-14 17:48:00 -0700

    add deployment, cluster, token, team and user handlers

--- import/import.go
+++ import/import.go
@@ -10,6 +10,8 @@ import (
 	"os/exec"
 	"strings"
 
+	"golang.org/x/exp/maps"
+
 	"github.com/astronomer/terraform-provider-astro/internal/clients/iam"
 
 	"github.com/astronomer/terraform-provider-astro/internal/clients"
@@ -99,14 +101,13 @@ func main() {
 	//	for each resource, we get the list of entities and generate the terraform import command
 
 	resourceHandlers := map[string]func(context.Context, *platform.ClientWithResponses, *iam.ClientWithResponses, string) string{
-		"workspace":                              handleWorkspaces,
-		"deployment":                             handleDeployments,
-		"cluster":                                handleClusters,
-		"hybrid_cluster_workspace_authorization": handleHybridClusterWorkspaceAuthorizations,
-		"api_token":                              handleApiTokens,
-		"team":                                   handleTeams,
-		"team_roles":                             handleTeamRoles,
-		"user_roles":                             handleUserRoles,
+		"workspace":  handleWorkspaces,
+		"deployment": handleDeployments,
+		"cluster":    handleClusters,
+		"api_token":  handleApiTokens,
+		"team":       handleTeams,
+		"team_roles": handleTeamRoles,
+		"user_roles": handleUserRoles,
 	}
 
 	for _, resource := range resources {
@@ -236,32 +237,358 @@ import {
 }
 
 func handleDeployments(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
+	log.Printf("Importing deployments for organization %s", organizationId)
 
-	return ""
+	deploymentsResp, err := platformClient.ListDeploymentsWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list deployments: %v", err)
+		return ""
+	}
+
+	// Check HTTP status code
+	if deploymentsResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", deploymentsResp.StatusCode())
+
+		// Try to read and log the response body
+		bodyString := string(deploymentsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if deploymentsResp.JSON200 == nil {
+		log.Printf("Failed to list deployments, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(deploymentsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, deploymentsResp.HTTPResponse, deploymentsResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	deployments := deploymentsResp.JSON200.Deployments
+	if deployments == nil {
+		log.Printf("Deployments list is nil")
+		return ""
+	}
+
+	deploymentIds := lo.Map(deployments, func(deployment platform.Deployment, _ int) string {
+		return deployment.Id
+	})
+	log.Printf("Importing Deployments: %v", deploymentIds)
+
+	var importString string
+	for _, deploymentId := range deploymentIds {
+		deploymentImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_deployment.deployment_%v
+}`, deploymentId, deploymentId)
+
+		importString += deploymentImportString + "\n"
+	}
+
+	return importString
 }
 
 func handleClusters(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
+	log.Printf("Importing clusters for organization %s", organizationId)
 
-	return ""
-}
+	clustersResp, err := platformClient.ListClustersWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list clusters: %v", err)
+		return ""
+	}
 
-func handleHybridClusterWorkspaceAuthorizations(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
+	// Check HTTP status code
+	if clustersResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", clustersResp.StatusCode())
 
-	return ""
+		// Try to read and log the response body
+		bodyString := string(clustersResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if clustersResp.JSON200 == nil {
+		log.Printf("Failed to list clusters, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(clustersResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, clustersResp.HTTPResponse, clustersResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	clusters := clustersResp.JSON200.Clusters
+	if clusters == nil {
+		log.Printf("Clusters list is nil")
+		return ""
+	}
+
+	var clusterMap map[string]platform.ClusterType
+	for _, cluster := range clusters {
+		clusterMap[cluster.Id] = cluster.Type
+	}
+
+	log.Printf("Importing Clusters: %v", maps.Keys(clusterMap))
+
+	var importString string
+	for clusterId, clusterType := range clusterMap {
+		clusterImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_cluster.cluster_%v
+}`, clusterId, clusterId)
+
+		// if clusterType is hybrid, we need to import the hybrid cluster workspace authorization
+		if clusterType == platform.ClusterTypeHYBRID {
+			clusterImportString += fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_hybrid_cluster_workspace_authorization.cluster_%v
+}`, clusterId, clusterId)
+		}
+
+		importString += clusterImportString + "\n"
+	}
+
+	return importString
 }
 
 func handleApiTokens(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
-	return ""
+	log.Printf("Importing API tokens for organization %s", organizationId)
+
+	apiTokensResp, err := iamClient.ListApiTokensWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list API tokens: %v", err)
+		return ""
+	}
+
+	// Check HTTP status code
+	if apiTokensResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", apiTokensResp.StatusCode())
+
+		// Try to read and log the response body
+		bodyString := string(apiTokensResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if apiTokensResp.JSON200 == nil {
+		log.Printf("Failed to list API tokens, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(apiTokensResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, apiTokensResp.HTTPResponse, apiTokensResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	apiTokens := apiTokensResp.JSON200.Tokens
+	if apiTokens == nil {
+		log.Printf("API tokens list is nil")
+		return ""
+	}
+
+	apiTokenIds := lo.Map(apiTokens, func(apiToken iam.ApiToken, _ int) string {
+		return apiToken.Id
+	})
+
+	log.Printf("Importing API Tokens: %v", apiTokenIds)
+
+	var importString string
+	for _, apiTokenId := range apiTokenIds {
+		apiTokenImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_api_token.api_token_%v
+}`, apiTokenId, apiTokenId)
+
+		importString += apiTokenImportString + "\n"
+	}
+
+	return importString
 }
 
 func handleTeams(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
-	return ""
+	log.Printf("Importing teams for organization %s", organizationId)
+
+	teamsResp, err := iamClient.ListTeamsWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list teams: %v", err)
+		return ""
+	}
+
+	// Check HTTP status code
+	if teamsResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", teamsResp.StatusCode())
+
+		// Try to read and log the response body
+		bodyString := string(teamsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if teamsResp.JSON200 == nil {
+		log.Printf("Failed to list teams, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(teamsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, teamsResp.HTTPResponse, teamsResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	teams := teamsResp.JSON200.Teams
+	if teams == nil {
+		log.Printf("Teams list is nil")
+		return ""
+	}
+
+	teamIds := lo.Map(teams, func(team iam.Team, _ int) string {
+		return team.Id
+	})
+
+	log.Printf("Importing Teams: %v", teamIds)
+
+	var importString string
+	for _, teamId := range teamIds {
+		teamImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_team.team_%v
+}`, teamId, teamId)
+
+		importString += teamImportString + "\n"
+	}
+
+	return importString
 }
 
 func handleTeamRoles(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
-	return ""
+	log.Printf("Importing team roles for organization %s", organizationId)
+
+	teamsResp, err := iamClient.ListTeamsWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list teams: %v", err)
+		return ""
+	}
+
+	// Check HTTP status code
+	if teamsResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", teamsResp.StatusCode())
+
+		// Try to read and log the response body
+		bodyString := string(teamsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if teamsResp.JSON200 == nil {
+		log.Printf("Failed to list teams, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(teamsResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, teamsResp.HTTPResponse, teamsResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	teams := teamsResp.JSON200.Teams
+	if teams == nil {
+		log.Printf("Teams list is nil")
+		return ""
+	}
+
+	teamIds := lo.Map(teams, func(team iam.Team, _ int) string {
+		return team.Id
+	})
+
+	log.Printf("Importing Team Roles: %v", teamIds)
+
+	var importString string
+	for _, teamId := range teamIds {
+		teamImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_team_roles.team_%v
+}`, teamId, teamId)
+
+		importString += teamImportString + "\n"
+	}
+
+	return importString
 }
 
 func handleUserRoles(ctx context.Context, platformClient *platform.ClientWithResponses, iamClient *iam.ClientWithResponses, organizationId string) string {
-	return ""
+	log.Printf("Importing user roles for organization %s", organizationId)
+
+	usersResp, err := iamClient.ListUsersWithResponse(ctx, organizationId, nil)
+	if err != nil {
+		log.Printf("Failed to list users: %v", err)
+		return ""
+	}
+
+	// Check HTTP status code
+	if usersResp.StatusCode() != http.StatusOK {
+		log.Printf("Unexpected status code: %d", usersResp.StatusCode())
+
+		// Try to read and log the response body
+		bodyString := string(usersResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	if usersResp.JSON200 == nil {
+		log.Printf("Failed to list users, JSON200 resp is nil, organizationId: %v", organizationId)
+		// Try to read and log the response body
+		bodyString := string(usersResp.Body)
+		log.Printf("Response body: %s", bodyString)
+		return ""
+	}
+
+	_, diagnostic := clients.NormalizeAPIError(ctx, usersResp.HTTPResponse, usersResp.Body)
+	if diagnostic != nil {
+		log.Printf("API Error diagnostic: %+v", diagnostic)
+	}
+
+	users := usersResp.JSON200.Users
+	if users == nil {
+		log.Printf("Users list is nil")
+		return ""
+	}
+
+	userIds := lo.Map(users, func(user iam.User, _ int) string {
+		return user.Id
+	})
+
+	log.Printf("Importing User Roles: %v", userIds)
+
+	var importString string
+	for _, userId := range userIds {
+		userImportString := fmt.Sprintf(`
+import {
+	id = "%v"
+	to = astro_user_roles.user_%v
+}`, userId, userId)
+
+		importString += userImportString + "\n"
+	}
+
+	return importString
 }
