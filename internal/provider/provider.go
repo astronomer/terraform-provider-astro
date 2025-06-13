cf1d5d15de95322d84b970fe5f2254d2c0409e98
Isaac Chung <isaacchung1990@gmail.com>
2025-06-13 16:24:37 -0700

    feat: add notification channel

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -151,6 +151,7 @@ func (p *AstroProvider) DataSources(ctx context.Context) []func() datasource.Dat
 		datasources.NewApiTokensDataSource,
 		datasources.NewAlertDataSource,
 		datasources.NewAlertsDataSource,
+		datasources.NewNotificationChannelDataSource,
 	}
 }
 
