c3cbd3537287ea16192980854547dde5d4354b68
Isaac Chung <isaacchung1990@gmail.com>
2026-01-09 14:42:40 -0800

    docs: improve example

--- examples/data-sources/astro_custom_role/data-source.tf
+++ examples/data-sources/astro_custom_role/data-source.tf
@@ -1,4 +1,8 @@
 data "astro_custom_role" "example" {
-  id = "your-custom-role-id"
+  id = "cmk64yvat027n01q7f9gn5ghg"
 }
 
+# Output the custom role value using terraform apply
+output "custom_role" {
+  value = data.astro_custom_role.example
+}
